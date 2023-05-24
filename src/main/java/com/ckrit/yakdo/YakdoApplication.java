f051beb612af75ada51dfdc9f782fae2becd32e3
CkriT_user <CkriT_user@CkriT>
2023-05-24 21:51:23 +0900

    first commit

---
+++ src/main/java/com/ckrit/yakdo/YakdoApplication.java
@@ -0,0 +1,13 @@
+package com.ckrit.yakdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YakdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YakdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ckrit/yakdo/YakdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ckrit.yakdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YakdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
